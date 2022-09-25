75742be05628528b375f0312f1eff4fc1e5e7396
Furkan Gundogan <furkan.gundogan@stu.fsm.edu.tr>
2022-09-25 21:12:22 +0300

    Folders created.

---
+++ src/navigators/SignNavigator.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const SignNavigator = () => {
+  return (
+    <View>
+      <Text>SignNavigator</Text>
+    </View>
+  )
+}
+
+export default SignNavigator
+
+const styles = StyleSheet.create({})
